0621ab40a08f5c547f571d0a20f7c8023c0aee94
deflwgt <126112078+deflwgt@users.noreply.github.com>
2024-12-15 17:50:28 +0700

    add buyer routes

--- controllers/buyer.controller.js
+++ controllers/buyer.controller.js
@@ -1,22 +1,33 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { v2 as cloudinary } from 'cloudinary'
-import dotenv from 'dotenv'
-dotenv.config()
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_SECRET_KEY,
 });
 
-import { findBuyerByEmail, createBuyer, findBuyer, findBuyerAndUpdate} from '../repositories/buyer.repository.js'
+import {
+  findBuyerByEmail,
+  createBuyer,
+  findBuyer,
+  findBuyerAndUpdate,
+} from "../repositories/buyer.repository.js";
 import { userStatus } from "../const/user.const.js";
+import {
+  getAllProductGroups,
+  getProductGroupById,
+} from "../repositories/productGroups.repository.js";
+import { getProductsByIds } from "../repositories/products.repository.js";
+
 //người mua đăng kí
-const buyerRegister = async (req, res) => {
+export const buyerRegister = async (req, res) => {
   const { name, email, password, phoneNumber } = req.body;
   try {
     const checkUser = await findBuyerByEmail(email);
-    if (checkUser) throw new Error(req.translate('user.emailExisted'));
+    if (checkUser) throw new Error(req.translate("user.emailExisted"));
 
     const salt = bcrypt.genSaltSync(+process.env.SALT_ROUNDS);
     const hash = bcrypt.hashSync(password, salt);
@@ -24,36 +35,40 @@ const buyerRegister = async (req, res) => {
       name,
       email,
       password: hash,
-      phoneNumber
+      phoneNumber,
     });
 
-    console.log("User registered:", newUser);  // Thêm log
+    console.log("User registered:", newUser); // Thêm log
 
     return res.status(201).send({
       message: "Created success",
-      newUser
+      newUser,
     });
   } catch (error) {
     return res.status(400).send({
-      message: error.message
+      message: error.message,
     });
   }
 };
 
-const buyerLogin = async (req, res) => {
+export const buyerLogin = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await findBuyer({ email });
-    console.log("User found:", user);  // Thêm log
-    
-    if (!user) throw new Error(req.translate('user.wrong'));
+    console.log("User found:", user); // Thêm log
 
-    const checkPassword = bcrypt.compareSync(password.toString(), user.password);
-    console.log("Password match:", checkPassword);  // Thêm log
-    
-    if (!checkPassword) throw new Error(req.translate('user.wrong'));
+    if (!user) throw new Error(req.translate("user.wrong"));
 
-    if(user.status == userStatus.inactive) throw new Error(req.translate('user.banned'));
+    const checkPassword = bcrypt.compareSync(
+      password.toString(),
+      user.password
+    );
+    console.log("Password match:", checkPassword); // Thêm log
+
+    if (!checkPassword) throw new Error(req.translate("user.wrong"));
+
+    if (user.status == userStatus.inactive)
+      throw new Error(req.translate("user.banned"));
 
     const { _id } = user;
     const accessToken = jwt.sign({ _id }, process.env.ACCESS_TK_KEY);
@@ -63,7 +78,7 @@ const buyerLogin = async (req, res) => {
       message: "login success",
       data: {
         accessToken,
-        refreshToken
+        refreshToken,
       },
     });
   } catch (error) {
@@ -74,13 +89,13 @@ const buyerLogin = async (req, res) => {
 };
 
 //profile người mua
-const buyerProfile = async (req, res) => {
+export const buyerProfile = async (req, res) => {
   try {
     const user = req.currentUser;
-    if(!user) throw new Error(req.translate('unauthorized'))
+    if (!user) throw new Error(req.translate("unauthorized"));
     res.status(200).send({
       data: {
-        user
+        user,
       },
     });
   } catch (error) {
@@ -90,17 +105,20 @@ const buyerProfile = async (req, res) => {
   }
 };
 //update profile người mua
-const buyerUpdateProfile = async (req, res) => {
-  const body = req.body
+export const buyerUpdateProfile = async (req, res) => {
+  const body = req.body;
   try {
-    const { _id }= req.currentUser
-    await findBuyerAndUpdate({
-      _id
-    },{
-      ...body
-    })
+    const { _id } = req.currentUser;
+    await findBuyerAndUpdate(
+      {
+        _id,
+      },
+      {
+        ...body,
+      }
+    );
     res.status(201).send({
-      message: req.translate('user.updateProfile'),
+      message: req.translate("user.updateProfile"),
     });
   } catch (error) {
     res.status(403).send({
@@ -108,4 +126,33 @@ const buyerUpdateProfile = async (req, res) => {
     });
   }
 };
-export {buyerLogin,buyerRegister, buyerProfile, buyerUpdateProfile}
+
+export const getProductGroups = async (req, res) => {
+  const { keyword, page, pageSize } = req.query;
+  try {
+    const data = await getAllProductGroups({ keyword, page, pageSize });
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+};
+
+export const getProductGroup = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const productGroup = await getProductGroupById(id);
+    if (!productGroup) {
+      throw new Error("Sản phẩm không tồn tại");
+    }
+    const products = await getProductsByIds(productGroup.productIds);
+
+    return res.status(200).send({
+      ...productGroup.toObject(),
+      products,
+    });
+  } catch (e) {
+    return res.status(404).send({ message: e.message });
+  }
+};
--- routes/productGroups.routes.js
+++ routes/productGroups.routes.js
@@ -1,6 +1,13 @@
 import multer from "multer";
 import { Router } from "express";
-import { createNewProductGroup, sellerGetProductGroups, sellerGetProductGroupById, sellerAddProduct, buyerGetProductGroups, buyerGetProductGroupById } from "../controllers/productGroups.controllers.js";
+import {
+  createNewProductGroup,
+  sellerGetProductGroups,
+  sellerGetProductGroupById,
+  sellerAddProduct,
+  buyerGetProductGroups,
+  buyerGetProductGroupById,
+} from "../controllers/productGroups.controllers.js";
 import sellerAuthMiddleware from "../middlewares/seller.auth.middleware.js";
 
 const storage = multer.memoryStorage();
@@ -8,14 +15,30 @@ const upload = multer({ storage: storage });
 
 const ProductGroupsRouter = Router();
 
-ProductGroupsRouter.get("/seller/products", sellerAuthMiddleware.authentication, sellerGetProductGroups);
-ProductGroupsRouter.post("/seller/create-productgroup", upload.single("file"), sellerAuthMiddleware.authentication, createNewProductGroup);
-ProductGroupsRouter.get("/seller/productgroup", sellerAuthMiddleware.authentication, sellerGetProductGroupById);
-ProductGroupsRouter.patch("/seller/add-product", sellerAuthMiddleware.authentication, sellerAddProduct)
+ProductGroupsRouter.get(
+  "/seller/products",
+  sellerAuthMiddleware.authentication,
+  sellerGetProductGroups
+);
+ProductGroupsRouter.post(
+  "/seller/create-productgroup",
+  upload.single("file"),
+  sellerAuthMiddleware.authentication,
+  createNewProductGroup
+);
+ProductGroupsRouter.get(
+  "/seller/productgroup",
+  sellerAuthMiddleware.authentication,
+  sellerGetProductGroupById
+);
+ProductGroupsRouter.patch(
+  "/seller/add-product",
+  sellerAuthMiddleware.authentication,
+  sellerAddProduct
+);
 // ProductGroupsRouter.get("/seller/product", sellerGetOneProduct)
 
-ProductGroupsRouter.get("/buyer/products", buyerGetProductGroups)
-ProductGroupsRouter.get("/buyer/productgroup", buyerGetProductGroupById)
-
+ProductGroupsRouter.get("/buyer/products", buyerGetProductGroups);
+ProductGroupsRouter.get("/buyer/productgroup", buyerGetProductGroupById);
 
 export default ProductGroupsRouter;
