3df187c0c82facffb224fbe86e38efbeb7215029
lmh05k68 <hle290105@gmail.com>
2024-12-29 01:21:59 +0700

    a

--- middlewares/buyer.auth.middlewares.js
+++ middlewares/buyer.auth.middlewares.js
@@ -8,11 +8,10 @@ const buyerAuthMiddleware = {
         const token = authToken && authToken.split(' ')[1]
         if(!token) throw new Error('Please login again')
         const verifyToken = jwt.verify(token, process.env.ACCESS_TK_KEY)
-        const { _id } = verifyToken
-        const user = await findBuyerById(_id)
+        const { id } = verifyToken
+        const user = await findBuyerById(id)
         if(!user) throw new Error('Unauthorized')
         req.currentUser = user
-
         next();
       } catch (error) {
         res.status(401).send({
